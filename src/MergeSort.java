1b946bf00daf68f31d055bac6f3e883a1f9f63a2
Xi Lu <enginlosopher@gmail.com>
2016-12-29 20:22:08 -0800

    c

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,29 +1,29 @@
 public class MergeSort {
-     
+
     private int[] array;
     private int[] tempMergArr;
     private int length;
- 
-    public static void main(String a[]){
-         
-        int[] inputArr = {45,23,11,89,77,98,4,28,65,43};
+
+    public static void main(String a[]) {
+
+        int[] inputArr = { 45, 23, 11, 89, 77, 98, 4, 28, 65, 43 };
         MergeSort mms = new MergeSort();
         mms.sort(inputArr);
-        for(int i:inputArr){
+        for (int i : inputArr) {
             System.out.print(i);
             System.out.print(" ");
         }
     }
-     
+
     public void sort(int inputArr[]) {
         this.array = inputArr;
         this.length = inputArr.length;
         this.tempMergArr = new int[length];
         doMergeSort(0, length - 1);
     }
- 
+
     private void doMergeSort(int lowerIndex, int higherIndex) {
-         
+
         if (lowerIndex < higherIndex) {
             int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
             // Below step sorts the left side of the array
@@ -34,15 +34,17 @@ public class MergeSort {
             mergeParts(lowerIndex, middle, higherIndex);
         }
     }
- 
+
     private void mergeParts(int lowerIndex, int middle, int higherIndex) {
- 
+
         for (int i = lowerIndex; i <= higherIndex; i++) {
             tempMergArr[i] = array[i];
         }
+
         int i = lowerIndex;
         int j = middle + 1;
         int k = lowerIndex;
+
         while (i <= middle && j <= higherIndex) {
             if (tempMergArr[i] <= tempMergArr[j]) {
                 array[k] = tempMergArr[i];
@@ -53,11 +55,18 @@ public class MergeSort {
             }
             k++;
         }
+
         while (i <= middle) {
             array[k] = tempMergArr[i];
             k++;
             i++;
         }
- 
+
+        while (j <= higherIndex) {
+            array[k] = tempMergArr[j];
+            k++;
+            j++;
+        }
+
     }
 }
