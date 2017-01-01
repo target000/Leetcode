fe016e44d026a3512e1477b7ef7d2865d6ecb7fc
Xi Lu <xlu@appbridge.ca>
2016-12-31 21:59:37 -0800

    c

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -3,6 +3,78 @@ import java.util.Map;
 
 class LRUCache {
 
+    private int capacity;
+    private Map<Integer, Node> map;
+    private Node head;
+    private Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<Integer, Node>();
+    }
+
+    public int get(int key) {
+
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            setHead(node);
+            return node.value;
+        }
+
+        return -1;
+    }
+
+    public void set(int key, int value) {
+        if (map.containsKey(key)) {
+            Node oldNode = map.get(key);
+            oldNode.value = value;
+            remove(oldNode);
+            setHead(oldNode);
+        } else {
+            Node newNode = new Node(key, value);
+            if (map.size() >= capacity) {
+                map.remove(tail.key);
+                remove(tail);
+            }
+            setHead(newNode);
+
+            map.put(key, newNode);
+        }
+    }
+
+    public void remove(Node node) {
+
+        if (node.pre != null) {
+            node.pre.next = node.next;
+        } else {
+            head = node.next;
+        }
+
+        if (node.next != null) {
+            node.next.pre = node.pre;
+        } else {
+            tail = node.pre;
+        }
+
+    }
+
+    public void setHead(Node node) {
+        node.next = head;
+        node.pre = null;
+
+        if (head != null) {
+            head.pre = node;
+        }
+
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+    }
+
+    // main test
     public static void main(String[] args) {
         LRUCache lruCache = new LRUCache(4);
 
@@ -17,78 +89,9 @@ class LRUCache {
 
     }
 
-    private int capacity;
-    private Map<Integer, Node> map = new HashMap<Integer, Node>();
-    private Node head = null;
-    private Node end = null;
-
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public int get(int key) {
-
-        if (map.containsKey(key)) {
-            Node n = map.get(key);
-            remove(n);
-            setHead(n);
-            return n.value;
-        }
-
-        return -1;
-    }
-
-    public void remove(Node n) {
-
-        if (n.pre != null) {
-            n.pre.next = n.next;
-        } else {
-            head = n.next;
-        }
-
-        if (n.next != null) {
-            n.next.pre = n.pre;
-        } else {
-            end = n.pre;
-        }
-
-    }
-
-    public void setHead(Node n) {
-        n.next = head;
-        n.pre = null;
-
-        if (head != null)
-            head.pre = n;
-
-        head = n;
-
-        if (end == null)
-            end = head;
-    }
-
-    public void set(int key, int value) {
-        if (map.containsKey(key)) {
-            Node old = map.get(key);
-            old.value = value;
-            remove(old);
-            setHead(old);
-        } else {
-            Node created = new Node(key, value);
-            if (map.size() >= capacity) {
-                map.remove(end.key);
-                remove(end);
-                setHead(created);
-
-            } else {
-                setHead(created);
-            }
-
-            map.put(key, created);
-        }
-    }
 }
 
+// doubly linked list
 class Node {
     int key;
     int value;
