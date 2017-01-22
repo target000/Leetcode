bf4a5b466b2f538af5765a7daa88f6f1ca1671df
Xi Lu <xlu@appbridge.ca>
2017-01-22 14:08:47 -0800

    c

--- Tree/SortedArr2Tree.java
+++ Tree/SortedArr2Tree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class SortedArr2Tree {
     public static void main(String[] args) {
@@ -6,7 +8,8 @@ public class SortedArr2Tree {
 
         Node root = arr2BST(arr);
 
-        inOrderTraversal(root);
+        int res = findSuccessor(root, 7);
+        System.out.println(res);
     }
 
     public static void inOrderTraversal(Node cur) {
@@ -15,6 +18,26 @@ public class SortedArr2Tree {
             System.out.println(cur.val);
             inOrderTraversal(cur.right);
         }
+    }
+
+    public static void inOrderTraversal2(Node cur, List<Integer> list, int i, int val) {
+        if (cur != null && i <= 1) {
+            inOrderTraversal2(cur.left, list, i, val);
+            list.add(cur.val);
+            if (cur.val == val) {
+                i++;
+            }
+            inOrderTraversal2(cur.right, list, i, val);
+        }
+    }
+
+    public static int findSuccessor(Node cur, int val) {
+        List<Integer> list = new ArrayList<>();
+
+        inOrderTraversal2(cur, list, 0, val);
+
+        int index = list.indexOf(val) + 1;
+        return (index < list.size() - 1) ? (int) list.get(index) : -1;
     }
 
     // preorder
