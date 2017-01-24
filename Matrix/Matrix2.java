575c39c5d0ffd69b667ecc682fe18d942f2aff9d
Xi Lu <xlu@appbridge.ca>
2017-01-23 23:49:47 -0800

    c

---
+++ Matrix/Matrix2.java
@@ -0,0 +1,22 @@
+
+public class Matrix2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length - 1;
+        int n = matrix[0].length - 1;
+
+        int i = m;
+        int j = 0;
+
+        while (i >= 0 && j <= n) {
+            if (target < matrix[i][j]) {
+                i--;
+            } else if (target > matrix[i][j]) {
+                j++;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
