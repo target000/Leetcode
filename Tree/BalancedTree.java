55795f463b1fe5aeb2716b56b8e2ed70dacf7ec5
Xi Lu <xlu@appbridge.ca>
2017-01-25 21:42:40 -0800

    c

---
+++ Tree/BalancedTree.java
@@ -0,0 +1,29 @@
+public class BalancedTree {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+
+        if (getHeight(root) == -1)
+            return false;
+
+        return true;
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+
+        if (left == -1 || right == -1)
+            return -1;
+
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return Math.max(left, right) + 1;
+
+    }
+}
