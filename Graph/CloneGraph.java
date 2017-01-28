435c2910cd69ba75c73dff0717a8338b834a88f7
Xi Lu <xlu@appbridge.ca>
2017-01-27 22:24:20 -0800

    c

--- Graph/CloneGraph.java
+++ Graph/CloneGraph.java
@@ -1,36 +1,43 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-class UndirectedGraphNode {
+// UndirectedNode
+class MyNode {
     int label;
-    ArrayList<UndirectedGraphNode> neighbors;
+    List<MyNode> neighbors;
 
-    UndirectedGraphNode(int x) {
+    MyNode(int x) {
         label = x;
-        neighbors = new ArrayList<UndirectedGraphNode>();
+        neighbors = new ArrayList<MyNode>();
     }
 }
 
 public class CloneGraph {
-    private Map<Integer, UndirectedGraphNode> map = new HashMap<>();
 
-    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    private Map<Integer, MyNode> map = new HashMap<>();
+
+    public MyNode cloneGraph(MyNode node) {
         return clone(node);
     }
 
-    private UndirectedGraphNode clone(UndirectedGraphNode node) {
+    private MyNode clone(MyNode node) {
+
         if (node == null)
             return null;
 
         if (map.containsKey(node.label)) {
             return map.get(node.label);
         }
-        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
+
+        MyNode clone = new MyNode(node.label);
         map.put(clone.label, clone);
-        for (UndirectedGraphNode neighbor : node.neighbors) {
+
+        for (MyNode neighbor : node.neighbors) {
             clone.neighbors.add(clone(neighbor));
         }
+
         return clone;
     }
 }
