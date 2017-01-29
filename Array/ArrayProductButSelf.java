a6d6ecefb5ce0693394a104421a7e9cb0a2ad0ea
Xi Lu <xlu@appbridge.ca>
2017-01-28 20:57:53 -0800

    c

--- Array/ArrayProductButSelf.java
+++ Array/ArrayProductButSelf.java
@@ -1,17 +1,32 @@
+// apparently the best way to do it is to mutiple all numbers and divide by current arr value
+// but it is against the requirement
+
+// basically do a forward sweep to cumulatively find the product
+// then do a backward sweep 
 
 public class ArrayProductButSelf {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
+
+    public int[] productExceptSelf(int[] arr) {
+
+        if (arr == null || arr.length == 0) {
+            return arr;
+        }
+
+        int n = arr.length;
         int[] res = new int[n];
         res[0] = 1;
+
         for (int i = 1; i < n; i++) {
-            res[i] = res[i - 1] * nums[i - 1];
+            res[i] = res[i - 1] * arr[i - 1];
         }
+
         int right = 1;
         for (int i = n - 1; i >= 0; i--) {
             res[i] *= right;
-            right *= nums[i];
+            right *= arr[i];
         }
+
         return res;
     }
+
 }
