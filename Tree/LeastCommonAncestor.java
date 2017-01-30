21f80f428efa3635532b00419cdcbde23161298e
Xi Lu <enginlosopher@gmail.com>
2017-01-29 20:35:26 -0800

    c

--- Tree/LeastCommonAncestor.java
+++ Tree/LeastCommonAncestor.java
@@ -14,15 +14,22 @@ public class LeastCommonAncestor {
         TreeNode right = lowestCommonAncestor(root.right, node1, node2);
 
         // Conquer
+
+        // the only way this can happen is that left and right both have root as common ancester
         if (left != null && right != null) {
             return root;
         }
+
+        // only left is not null but right is null meaning that nodes have LCA of left
         if (left != null) {
             return left;
         }
+
+        // vice versa
         if (right != null) {
             return right;
         }
+        // last case both are null then it is a leaf
         return null;
     }
 
